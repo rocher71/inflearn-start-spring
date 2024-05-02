e36c4fff9417644c9b192868a3de6bebd2b6992d
rocher71 <qja086351@naver.com>
2024-05-03 00:43:10 +0900

    4. 스프링 빈과 의존관계 - 복습

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,7 +9,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired
+    //spring container한테 등록하고 사용, 매번 new memberService 선언하지 않도록. 
+    @Autowired  // -> spring container가 뜰 때 MemberController를 생성, 이 생성자를 호출.
+    // @Autowired가 있으면 parameter의 memberService를 Spring이 SpringContainer에 있는 memberService를 가져다가 연결시켜줌.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
